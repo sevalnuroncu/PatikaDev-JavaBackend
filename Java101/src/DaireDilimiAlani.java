dca2ea3ea18c697ef25202eef7559b6621eb3bf5
sevalnuroncu <sevalnuroncu@gmail.com>
2023-01-19 22:50:15 +0300

    daire diliminin alanını bulma

--- Java101/src/DaireDilimiAlani.java
+++ Java101/src/DaireDilimiAlani.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DaireDilimiAlanı {
+public class DaireDilimiAlani {
     public static void main(String[] args) {
         /**
          * Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
@@ -8,14 +8,14 @@ public class DaireDilimiAlanı {
          * Formül : (𝜋 * (r*r) * 𝛼) / 360
          */
         int yaricap, merkezaci;
-        double pi=3.14, dilimAlanı;
+        double pi=3.14, dilimAlani;
         Scanner input=new Scanner(System.in);
         System.out.print("yarıçapı giriniz: ");
         yaricap=input.nextInt();
         System.out.print("merkez açısını giriniz: ");
         merkezaci=input.nextInt();
-        dilimAlanı=(pi*yaricap*yaricap*merkezaci)/360;
-        System.out.println("Daire diliminin alanı= "+dilimAlanı);
+        dilimAlani=(pi*yaricap*yaricap*merkezaci)/360;
+        System.out.println("Daire diliminin alanı= "+dilimAlani);
 
 
     }
