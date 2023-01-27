f3c0d11a3f6481155746dd4f25771db5421ea6c7
sevalnuroncu <sevalnuroncu@gmail.com>
2023-01-27 13:09:05 +0300

    Üç basmaklı armstrong sayılar

---
+++ Java101/src/UcBasamakliArmstrongSayilari.java
@@ -0,0 +1,34 @@
+public class UcBasamakliArmstrongSayilari {
+    public static void main(String[] args) {
+        /**
+         * Üç basamaklı armstrong sayıları bulup ekrana yazdıran program
+         */
+       int basNumber, basValue, tempNumber,basPow,result;
+       for(int number=100; number<1000; number++){
+            result=0;
+            basNumber=0;
+            tempNumber=number;
+            while(tempNumber!=0){
+                tempNumber/=10;
+                basNumber++;
+            }
+            tempNumber=number;
+            while(tempNumber!=0){
+                basValue=tempNumber%10;
+                basPow=1;
+                for(int i=1; i<=basNumber; i++){
+                    basPow*=basValue;
+                }
+                result+=basPow;
+                tempNumber/=10;
+
+            }
+            if (result==number){
+                System.out.println(number +" sayısı bir Armstrong sayısıdır");
+            }
+
+
+
+       }
+    }
+}
